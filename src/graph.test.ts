fccf7df25b9297b3c9ea75898fcf44b4ab656816
Ciaran Ashton <ciaranashton@me.com>
2019-05-15 01:43:43 +0100

    Code style changes

--- src/graph.test.ts
+++ src/graph.test.ts
@@ -1,149 +1,149 @@
 import { Graph } from './graph';
 
 describe('Graph - datastructure', () => {
-    test('Add graph nodes and edge', () => {
-        const g = new Graph();
-        g.addNode('a');
+  test('Add graph nodes and edge', () => {
+    const g = new Graph();
+    g.addNode('a');
 
-        expect(g.getNode('a').key).toEqual('a');
-        expect(g.getNode('a').neighbours).toEqual([]);
+    expect(g.getNode('a').key).toEqual('a');
+    expect(g.getNode('a').neighbours).toEqual([]);
 
-        g.addNode('b');
-        g.addEdge('a', 'b', 5);
+    g.addNode('b');
+    g.addEdge('a', 'b', 5);
 
-        expect(g.getEdge('a', 'b')).toEqual(5);
-        expect(g.getNode('a').neighbours[0].key).toEqual('b');
-    });
+    expect(g.getEdge('a', 'b')).toEqual(5);
+    expect(g.getNode('a').neighbours[0].key).toEqual('b');
+  });
 });
 
 describe('Graph - path finding', () => {
-    const g = new Graph();
+  const g = new Graph();
 
-    const nodes = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-    const routes = [
-        [['a', 'c'], 2],
-        [['c', 'd'], 1],
-        [['c', 'f'], 4],
-        [['b', 'd'], 4],
-        [['b', 'e'], 7],
-        [['d', 'f'], 1],
-        [['d', 'g'], 1],
-        [['f', 'g'], 3],
-        [['g', 'h'], 4],
-        [['e', 'h'], 10],
-    ];
+  const nodes = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+  const routes = [
+    [['a', 'c'], 2],
+    [['c', 'd'], 1],
+    [['c', 'f'], 4],
+    [['b', 'd'], 4],
+    [['b', 'e'], 7],
+    [['d', 'f'], 1],
+    [['d', 'g'], 1],
+    [['f', 'g'], 3],
+    [['g', 'h'], 4],
+    [['e', 'h'], 10],
+  ];
 
-    nodes.forEach(node => g.addNode(node));
-    routes.forEach(([routes, distance]: any[]) => {
-        const [start, destination] = routes;
-        g.addEdge(start, destination, distance);
-    });
+  nodes.forEach(node => g.addNode(node));
+  routes.forEach(([routes, distance]: any[]) => {
+    const [start, destination] = routes;
+    g.addEdge(start, destination, distance);
+  });
 
-    test('dijkstra - a', () => {
-        const expectedResults = {
-            a: { distance: 0, previous: null },
-            b: { distance: 7, previous: 'd' },
-            c: { distance: 2, previous: 'a' },
-            d: { distance: 3, previous: 'c' },
-            e: { distance: 14, previous: 'b' },
-            f: { distance: 4, previous: 'd' },
-            g: { distance: 4, previous: 'd' },
-            h: { distance: 8, previous: 'g' },
-        };
+  test('dijkstra - a', () => {
+    const expectedResults = {
+      a: { distance: 0, previous: null },
+      b: { distance: 7, previous: 'd' },
+      c: { distance: 2, previous: 'a' },
+      d: { distance: 3, previous: 'c' },
+      e: { distance: 14, previous: 'b' },
+      f: { distance: 4, previous: 'd' },
+      g: { distance: 4, previous: 'd' },
+      h: { distance: 8, previous: 'g' },
+    };
 
-        expect(g.dijkstra('a')).toEqual(expectedResults);
-    });
+    expect(g.dijkstra('a')).toEqual(expectedResults);
+  });
 
-    test('dijkstra - e', () => {
-        const expectedResults = {
-            a: { distance: 14, previous: 'c' },
-            b: { distance: 7, previous: 'e' },
-            c: { distance: 12, previous: 'd' },
-            d: { distance: 11, previous: 'b' },
-            e: { distance: 0, previous: null },
-            f: { distance: 12, previous: 'd' },
-            g: { distance: 12, previous: 'd' },
-            h: { distance: 10, previous: 'e' },
-        };
+  test('dijkstra - e', () => {
+    const expectedResults = {
+      a: { distance: 14, previous: 'c' },
+      b: { distance: 7, previous: 'e' },
+      c: { distance: 12, previous: 'd' },
+      d: { distance: 11, previous: 'b' },
+      e: { distance: 0, previous: null },
+      f: { distance: 12, previous: 'd' },
+      g: { distance: 12, previous: 'd' },
+      h: { distance: 10, previous: 'e' },
+    };
 
-        expect(g.dijkstra('e')).toEqual(expectedResults);
-    });
+    expect(g.dijkstra('e')).toEqual(expectedResults);
+  });
 
-    test('A -> F', () => {
-        const expected = ['a', 'c', 'd', 'f'];
-        const result = g.findPath('a', 'f');
+  test('A -> F', () => {
+    const expected = ['a', 'c', 'd', 'f'];
+    const result = g.findPath('a', 'f');
 
-        expect(result).toEqual(expected);
-    });
+    expect(result).toEqual(expected);
+  });
 
-    test('A -> H', () => {
-        const expected = ['a', 'c', 'd', 'g', 'h'];
-        const result = g.findPath('a', 'h');
+  test('A -> H', () => {
+    const expected = ['a', 'c', 'd', 'g', 'h'];
+    const result = g.findPath('a', 'h');
 
-        expect(result).toEqual(expected);
-    });
+    expect(result).toEqual(expected);
+  });
 
-    test('B -> H', () => {
-        const expected = ['b', 'd', 'g', 'h'];
-        const result = g.findPath('b', 'h');
+  test('B -> H', () => {
+    const expected = ['b', 'd', 'g', 'h'];
+    const result = g.findPath('b', 'h');
 
-        expect(result).toEqual(expected);
-    });
+    expect(result).toEqual(expected);
+  });
 
-    test('H -> D', () => {
-        const expected = ['h', 'g', 'd'];
-        const result = g.findPath('h', 'd');
+  test('H -> D', () => {
+    const expected = ['h', 'g', 'd'];
+    const result = g.findPath('h', 'd');
 
-        expect(result).toEqual(expected);
-    });
+    expect(result).toEqual(expected);
+  });
 
-    test('throw error when given unknown source node', () => {
-        expect(() => g.findPath('z', 'd')).toThrowError(
-            'Could not find source node z',
-        );
-    });
+  test('throw error when given unknown source node', () => {
+    expect(() => g.findPath('z', 'd')).toThrowError(
+      'Could not find source node z',
+    );
+  });
 
-    test('throw error when given unknown destination node', () => {
-        expect(() => g.findPath('a', 'z')).toThrowError(
-            'Could not find source node z',
-        );
-    });
+  test('throw error when given unknown destination node', () => {
+    expect(() => g.findPath('a', 'z')).toThrowError(
+      'Could not find source node z',
+    );
+  });
 });
 
 describe('Graph - traversing and searching', () => {
-    const graph = new Graph();
-    const nodes = ['a', 'b', 'c', 'd', 'e', 'f'];
-    const edges = [
-        ['a', 'b'],
-        ['a', 'e'],
-        ['a', 'f'],
-        ['b', 'd'],
-        ['b', 'e'],
-        ['c', 'b'],
-        ['d', 'c'],
-        ['d', 'e'],
-    ];
+  const graph = new Graph();
+  const nodes = ['a', 'b', 'c', 'd', 'e', 'f'];
+  const edges = [
+    ['a', 'b'],
+    ['a', 'e'],
+    ['a', 'f'],
+    ['b', 'd'],
+    ['b', 'e'],
+    ['c', 'b'],
+    ['d', 'c'],
+    ['d', 'e'],
+  ];
 
-    nodes.map(node => graph.addNode(node));
-    edges.map(nodes => graph.addEdge(nodes[0], nodes[1], 0));
+  nodes.map(node => graph.addNode(node));
+  edges.map(nodes => graph.addEdge(nodes[0], nodes[1], 0));
 
-    test('bredth first search', () => {
-        const order = ['a', 'b', 'e', 'f', 'd', 'c'];
-        let i = 0;
+  test('bredth first search', () => {
+    const order = ['a', 'b', 'e', 'f', 'd', 'c'];
+    let i = 0;
 
-        graph.bfs('a', (node: any) => {
-            expect(node.key).toEqual(order[i]);
-            i++;
-        });
+    graph.bfs('a', (node: any) => {
+      expect(node.key).toEqual(order[i]);
+      i++;
     });
+  });
 
-    test('depth first search', () => {
-        const order = ['a', 'b', 'd', 'c', 'e', 'f'];
-        let i = 0;
+  test('depth first search', () => {
+    const order = ['a', 'b', 'd', 'c', 'e', 'f'];
+    let i = 0;
 
-        graph.dfs('a', (node: any) => {
-            expect(node.key).toEqual(order[i]);
-            i++;
-        });
+    graph.dfs('a', (node: any) => {
+      expect(node.key).toEqual(order[i]);
+      i++;
     });
+  });
 });
--- src/graph.ts
+++ src/graph.ts
@@ -1,179 +1,159 @@
 interface Result {
-    distance: number;
-    previous: string | null;
+  distance: number;
+  previous: string | null;
 }
 
 class Node {
-    public key: string;
-    public neighbours: Node[];
+  public key: string;
+  public neighbours: Node[];
 
-    constructor(key: string) {
-        this.key = key;
-        this.neighbours = [];
-    }
+  constructor(key: string) {
+    this.key = key;
+    this.neighbours = [];
+  }
 
-    public addNeighbour(node: Node) {
-        this.neighbours.push(node);
-    }
+  public addNeighbour(node: Node) {
+    this.neighbours.push(node);
+  }
 }
 
 export class Graph {
-    private nodes: Node[];
-    private edges: Map<string, number>;
+  private nodes: Node[];
+  private edges: Map<string, number>;
 
-    constructor() {
-        this.nodes = [];
-        this.edges = new Map();
+  constructor() {
+    this.nodes = [];
+    this.edges = new Map();
+  }
+
+  public addNode(key: string): void {
+    this.nodes.push(new Node(key));
+  }
+
+  public getNode(key: string): Node {
+    const node = this.nodes.find(node => node.key === key);
+    if (!node) {
+      throw new Error(`Could not find source node ${key}`);
     }
+    return node;
+  }
 
-    public addNode(key: string): void {
-        this.nodes.push(new Node(key));
-    }
+  public addEdge(key1: string, key2: string, weight: number): void {
+    const startNode = this.getNode(key1);
+    const endNode = this.getNode(key2);
 
-    public getNode(key: string): Node {
-        const node = this.nodes.find(node => node.key === key);
-        if (!node) {
-            throw new Error(`Could not find source node ${key}`);
+    const [sort1, sort2] = [key1, key2].sort((a, b) => a.localeCompare(b));
+    this.edges.set(`${sort1}-${sort2}`, weight);
+
+    startNode.addNeighbour(endNode);
+    endNode.addNeighbour(startNode);
+  }
+
+  public getEdge(key1: string, key2: string): number | undefined {
+    const [sort1, sort2] = [key1, key2].sort((a, b) => a.localeCompare(b));
+    return this.edges.get(`${sort1}-${sort2}`);
+  }
+
+  private findLowest(
+    dist: { [key: string]: Result },
+    visited: Set<string>,
+  ): [string, Result] {
+    return Object.entries(dist).reduce(
+      (acc: [string, Result], val: [string, Result]): [string, Result] =>
+        val[1].distance < acc[1].distance && !visited.has(val[0]) ? val : acc,
+      ['', { distance: Infinity, previous: null }],
+    );
+  }
+
+  public dijkstra(source: string) {
+    const results: { [key: string]: Result } = {};
+    const visited: Set<string> = new Set();
+
+    this.nodes.forEach(({ key }) => {
+      results[key] = {
+        distance: Infinity,
+        previous: null,
+      };
+    });
+    results[source] = {
+      distance: 0,
+      previous: null,
+    };
+
+    while (visited.size < this.nodes.length) {
+      const [lowestKey] = this.findLowest(results, visited);
+      visited.add(lowestKey);
+
+      const node = this.getNode(lowestKey);
+      node.neighbours.forEach(neighbour => {
+        if (visited.has(neighbour.key)) return;
+
+        const edgeWeight = this.getEdge(node.key, neighbour.key);
+        const alt = results[node.key].distance + (edgeWeight || Infinity);
+
+        if (alt < results[neighbour.key].distance) {
+          results[neighbour.key] = {
+            distance: alt,
+            previous: node.key,
+          };
         }
-        return node;
+      });
     }
 
-    public addEdge(key1: string, key2: string, weight: number): void {
-        const startNode = this.getNode(key1);
-        const endNode = this.getNode(key2);
+    return results;
+  }
 
-        const [sort1, sort2] = [key1, key2].sort((a, b) =>
-            a.localeCompare(b),
-        );
-        this.edges.set(`${sort1}-${sort2}`, weight);
+  public findPath(source: string, destination: string): string[] {
+    if (!this.nodes.find(({ key }) => key === destination)) {
+      throw new Error(`Could not find source node ${destination}`);
+    }
+    const results = this.dijkstra(source);
 
-        startNode.addNeighbour(endNode);
-        endNode.addNeighbour(startNode);
+    function buildRoute(key: string, route: string[] = []): string[] {
+      const { previous } = results[key];
+      return previous === null
+        ? [key, ...route]
+        : buildRoute(previous, [key, ...route]);
     }
 
-    public getEdge(key1: string, key2: string): number | undefined {
-        const [sort1, sort2] = [key1, key2].sort((a, b) =>
-            a.localeCompare(b),
-        );
-        return this.edges.get(`${sort1}-${sort2}`);
-    }
+    return buildRoute(destination);
+  }
 
-    private findLowest(
-        dist: { [key: string]: Result },
-        visited: Set<string>,
-    ): [string, Result] {
-        return Object.entries(dist).reduce(
-            (
-                acc: [string, Result],
-                val: [string, Result],
-            ): [string, Result] =>
-                val[1].distance < acc[1].distance &&
-                !visited.has(val[0])
-                    ? val
-                    : acc,
-            ['', { distance: Infinity, previous: null }],
-        );
-    }
+  public bfs(startKey: string, fn: Function) {
+    const visited: Set<string> = new Set();
+    const q: Node[] = [];
+    q.unshift(this.getNode(startKey));
 
-    public dijkstra(source: string) {
-        const results: { [key: string]: Result } = {};
-        const visited: Set<string> = new Set();
+    while (q.length > 0) {
+      const currentNode = q.pop();
 
-        this.nodes.forEach(({ key }) => {
-            results[key] = {
-                distance: Infinity,
-                previous: null,
-            };
-        });
-        results[source] = {
-            distance: 0,
-            previous: null,
-        };
+      if (!visited.has(currentNode!.key)) {
+        fn(currentNode);
+        visited.add(currentNode!.key);
+      }
 
-        while (visited.size < this.nodes.length) {
-            const [lowestKey] = this.findLowest(results, visited);
-            visited.add(lowestKey);
-
-            const node = this.getNode(lowestKey);
-            node.neighbours.forEach(neighbour => {
-                if (visited.has(neighbour.key)) return;
-
-                const edgeWeight = this.getEdge(
-                    node.key,
-                    neighbour.key,
-                );
-                const alt =
-                    results[node.key].distance +
-                    (edgeWeight || Infinity);
-
-                if (alt < results[neighbour.key].distance) {
-                    results[neighbour.key] = {
-                        distance: alt,
-                        previous: node.key,
-                    };
-                }
-            });
+      currentNode!.neighbours.forEach(node => {
+        if (!visited.has(node.key)) {
+          q.unshift(node);
         }
+      });
+    }
+  }
 
-        return results;
+  public dfs(startKey: string, fn: Function) {
+    const visited: Set<string> = new Set();
+
+    function explore(node: Node) {
+      if (visited.has(node.key)) return;
+
+      fn(node);
+      visited.add(node.key);
+
+      node.neighbours.forEach(node => {
+        explore(node);
+      });
     }
 
-    public findPath(source: string, destination: string): string[] {
-        if (!this.nodes.find(({ key }) => key === destination)) {
-            throw new Error(
-                `Could not find source node ${destination}`,
-            );
-        }
-        const results = this.dijkstra(source);
-
-        function buildRoute(
-            key: string,
-            route: string[] = [],
-        ): string[] {
-            const { previous } = results[key];
-            return previous === null
-                ? [key, ...route]
-                : buildRoute(previous, [key, ...route]);
-        }
-
-        return buildRoute(destination);
-    }
-
-    public bfs(startKey: string, fn: Function) {
-        const visited: Set<string> = new Set();
-        const q: Node[] = [];
-        q.unshift(this.getNode(startKey));
-
-        while (q.length > 0) {
-            const currentNode = q.pop();
-
-            if (!visited.has(currentNode!.key)) {
-                fn(currentNode);
-                visited.add(currentNode!.key);
-            }
-
-            currentNode!.neighbours.forEach(node => {
-                if (!visited.has(node.key)) {
-                    q.unshift(node);
-                }
-            });
-        }
-    }
-
-    public dfs(startKey: string, fn: Function) {
-        const visited: Set<string> = new Set();
-
-        function explore(node: Node) {
-            if (visited.has(node.key)) return;
-
-            fn(node);
-            visited.add(node.key);
-
-            node.neighbours.forEach(node => {
-                explore(node);
-            });
-        }
-
-        explore(this.getNode(startKey));
-    }
+    explore(this.getNode(startKey));
+  }
 }
